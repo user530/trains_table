93514b77f1e5627e96612fcf0e0207df31a5cb01
user530 <m.timarsuev@gmail.com>
2024-05-11 16:58:17 +0300

    Characteristics table update data button and placeholder logic

--- src/features/characteristics/components/Characteristics.tsx
+++ src/features/characteristics/components/Characteristics.tsx
@@ -1,20 +1,38 @@
 import { useAppSelector } from '../../../app/hooks'
 import { selectChosenTrain } from '../../trains/trainsSlice'
+import { selectAllCharacteristics, selectCharacteristicsErrors } from '../characteristicsSlice';
 import { CharacteristicsTable } from './CharacteristicsTable';
 
 export const Characteristics = () => {
+    console.log('Characteristics rendered');
     const selectedTrain = useAppSelector(selectChosenTrain);
+    const characteristics = useAppSelector(selectAllCharacteristics);
+    const charErrors = useAppSelector(selectCharacteristicsErrors);
 
     if (!selectedTrain)
         return <></>
 
     const { name, description } = selectedTrain;
 
+    const btnClickHandler = () => {
+        const sortedCharacteristics = [...characteristics].sort(
+            (firstChar, secondChar) => (firstChar.speed - secondChar.speed)
+        );
+
+        console.log(sortedCharacteristics);
+    }
+
     return (
         <div>
             <h2>{name}</h2>
             <h5>{description}</h5>
+
             <CharacteristicsTable />
+
+            <button
+                disabled={charErrors.length > 0}
+                onClick={btnClickHandler}
+            >Отправить данные</button>
         </div>
     )
 }
