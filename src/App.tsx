0a8b7d5dbcc09f9e36405082aed0c5316e4ac330
user530 <m.timarsuev@gmail.com>
2024-05-12 12:20:36 +0300

    Updated App + cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
-import { TrainsTable } from './features/trains/components/TrainsTable/TrainsTable';
+import { Characteristics } from './features/characteristics/components/Characteristics';
+import { Trains } from './features/trains/components/Trains';
 
 function App() {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header> */}
-      <TrainsTable />
+    <div className='App h-screen bg-gray-200'>
+      <div className='container mx-auto px-4 py-4'>
+        <h1 className='text-5xl font-bold text-center py-5'>Данные поездов</h1>
+        <div className='flex gap-10'>
+          <Trains />
+
+          <Characteristics />
+        </div>
+      </div>
     </div>
   );
 }
